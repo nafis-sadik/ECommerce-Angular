27fc40ab9cad78c06e4d674677c7b923ed279f25
nafis-sadik <nafis_sadik@outlook.com>
2021-03-08 12:05:22 +0600

    Angular material version issue resolved

--- src/app/shared-module/shared-module.module.ts
+++ src/app/shared-module/shared-module.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { SharedModuleRoutingModule } from './shared-module-routing.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [NavBarComponent],
   imports: [
     CommonModule,
     SharedModuleRoutingModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule
   ],
   exports:[
     NavBarComponent,
